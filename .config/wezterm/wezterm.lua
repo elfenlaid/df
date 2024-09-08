11a686795d79648529da9877fca24cc071e66280
Egor Mihnevich <mikhnevich.egor@gmail.com>
2024-09-08 12:14:13 +0200

    update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
 config.color_scheme = 'Tokyo Night'
 -- Choose your favourite font, make sure it's installed on your machine
 config.font = wezterm.font({ family = 'JetbrainsMono Nerd Font' })
